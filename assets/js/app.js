2ec00f4d58b81ae90f039458a4c0ee31d5d6ec47
Viju.vastrad@n-andgroup.com <viju.vastrad@n-andgroup.com>
2019-10-29 11:45:36 +0000

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+ 
+ $(document).ready(function(){
+    // setInterval(function () {
+        // $('#data-display-1').delay(2000).fadeIn().delay(2000).fadeOut();
+        $('#data-display-2').delay(2000).fadeIn();
+    // },8000)
+  
+ });
+
+ //homepage carousel slider 
+ $('#image-slides').owlCarousel({
+     loop: true,
+     autoplay: true,
+     slideSpeed: 10000,
+     nav: false,
+     responsive: {
+         0: {
+             items: 1
+         },
+         600: {
+             items: 1
+         },
+         1000: {
+             items: 1
+         }
+     }
+ })
+
+ //footer breaking news date and time
+ function clock() {
+     const fullDate = new Date();
+     var day = fullDate.getDate();
+     var month = fullDate.getMonth();
+     var year = fullDate.getFullYear();
+     var hour = fullDate.getHours();
+     var minutes = fullDate.getMinutes();
+     var sec = fullDate.getSeconds();
+
+     if (hour < 10) {
+         hour = "0" + hour;
+     }
+     if (minutes < 10) {
+         minutes = "0" + minutes;
+     }
+     if (sec < 10) {
+         sec = "0" + sec;
+     }
+     if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+     document.getElementById('date').innerHTML=day;
+     document.getElementById('month').innerHTML="/"+month;
+     document.getElementById('year').innerHTML="/"+year;
+
+     document.getElementById('hours').innerHTML = hour;
+     document.getElementById('minutes').innerHTML = ":" + minutes;
+     document.getElementById('seconds').innerHTML = ":" + sec;
+
+ }
+ setInterval(clock, 100);
